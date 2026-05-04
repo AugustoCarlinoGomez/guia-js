5fdf0885648952e8f24aa3162daa33bb8fb67327
AugustoCarlinoGomez <augusterminator@gmail.com>
2026-05-04 19:46:51 -0300

    act7

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ let penalizacion = 20;
 
 let puntajeTotal = puntosBase + bonus - penalizacion;
 console.log("Puntaje total:", puntajeTotal);
+
+const input = document.getElementById("dato");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+ const valor = input.value;
+
+ const numero = Number(valor);   // convertir a número
+ const resultado = numero + 10;  // sumar 10
+
+ mensaje.textContent = resultado; // mostrar en pantalla
+});
